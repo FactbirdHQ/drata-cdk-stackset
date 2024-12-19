56a17d414f98083dac238e69290476cbb93f3174
Martin Madsen <mj@factbird.com>
2024-12-19 17:14:18 +0100

    Clean up flake.nix and add alejandra and statix

--- flake.nix
+++ flake.nix
@@ -2,43 +2,50 @@
   inputs = {
     # Update lockfile with `nix flake lock --update-input <input>`
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    systems.url = "github:nix-systems/default";
     devenv = {
       url = "github:cachix/devenv";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, devenv, systems, ... } @ inputs:
-
-    let
-      eachSystem = nixpkgs.lib.genAttrs (import systems);
-      system = builtins.currentSystem;
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
-      {
-        devShell.${system} = devenv.lib.mkShell {
-          inherit inputs pkgs;
-          modules = [
-            {
-              packages = [
-                pkgs.nodejs_23
-                pkgs.nodePackages.yarn
-              ];
-
-              pre-commit.hooks = {
-                biome = {
-                  package = pkgs.biome;
-                  enable = true;
-                  entry = "${pkgs.biome}/bin/biome check --write --no-errors-on-unmatched --diagnostic-level=error --verbose";
-        };
-        typos = {
-          enable = true;
-          entry = "${pkgs.typos}/bin/typos --force-exclude --exclude .git/*";
-        };
-              };
-            }
+  outputs = {
+    self,
+    nixpkgs,
+    devenv,
+    ...
+  } @ inputs: let
+    system = builtins.currentSystem;
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShell.${system} = devenv.lib.mkShell {
+      inherit inputs pkgs;
+      modules = [
+        {
+          packages = [
+            pkgs.nodejs_23
+            pkgs.nodePackages.yarn
           ];
-        };
-      };
+
+          pre-commit.hooks = {
+            alejandra.enable = true;
+            statix = {
+              enable = true;
+              pass_filenames = true;
+              # https://github.com/oppiliappan/statix/issues/69
+              entry = "bash -c 'echo \"$@\" | xargs -n1 ${pkgs.statix}/bin/statix check'";
+            };
+            biome = {
+              package = pkgs.biome;
+              enable = true;
+              entry = "${pkgs.biome}/bin/biome check --write --no-errors-on-unmatched --diagnostic-level=error --verbose";
+            };
+            typos = {
+              enable = true;
+              entry = "${pkgs.typos}/bin/typos --force-exclude --exclude .git/*";
+            };
+          };
+        }
+      ];
+    };
+  };
 }
